f8da0fff7bf898bccfb9b35f8e0c5688cce3aba4
Quentin Debroux <132827056+QuentinDebroux@users.noreply.github.com>
2023-05-08 19:56:11 +0200

    Level2 added

---
+++ app/src/main/java/com/example/logicgateappproject/levels/Level2.kt
@@ -0,0 +1,42 @@
+package com.example.logicgateappproject.levels
+
+import android.os.Bundle
+import com.example.logicgateappproject.operators.*
+
+class Level2: Level() {
+    private var h: Float = 1f
+    private var w: Float = 1f
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        h = levelView.screenHeight
+        w = levelView.screenWidth
+        levelView.levelName = "Level 2"
+    }
+
+    override fun declareOperators() {
+        //Create operators and put them in the list
+        levelView.operators.add(ExactLight(w/2, h/4, this, 1, 1))
+
+        levelView.operators.add(And(w/2, h/2, this))
+
+        levelView.operators.add(NormalSwitch(w/3, 3*h/4, this))
+        levelView.operators.add(NormalSwitch(2*w/3, 3*h/4, this))
+
+        //Connect operators
+        connectOperators(output = levelView.operators[0],input = levelView.operators[1])
+        connectOperators(output = levelView.operators[1],input = levelView.operators[2])
+        connectOperators(output = levelView.operators[1],input = levelView.operators[3])
+
+        thread = Thread(Runnable {
+            while (true) {
+                if (levelView.operators[0].state == 1) {
+                    levelView.win = true
+                    println("win")
+                    break
+                }
+            }
+        })
+        thread.start()
+    }
+}
