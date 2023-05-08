ec7772764fd8b459515ec867bfe363e34670cd94
Quentin Debroux <132827056+QuentinDebroux@users.noreply.github.com>
2023-05-08 19:53:03 +0200

    Update Level1.kt

--- app/src/main/java/com/example/logicgateappproject/levels/Level1.kt
+++ app/src/main/java/com/example/logicgateappproject/levels/Level1.kt
@@ -1,16 +1,18 @@
 package com.example.logicgateappproject.levels
 
+import android.os.Bundle
 import com.example.logicgateappproject.operators.*
 
 class Level1: Level() {
     private var h: Float = 1f
     private var w: Float = 1f
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         h = levelView.screenHeight
         w = levelView.screenWidth
-    }*/
+        levelView.levelName = "Level 1"
+    }
 
     override fun declareOperators() {
         //Create operators and put them in the list
@@ -19,13 +21,22 @@ class Level1: Level() {
         levelView.operators.add(And(w/2, h/2, this))
 
         levelView.operators.add(NormalSwitch(w/3, 3*h/4, this))
-        //println(levelView.operators[2].hitbox.top)
-        //println(levelView.operators[2].posY)
         levelView.operators.add(NormalSwitch(2*w/3, 3*h/4, this))
 
         //Connect operators
         connectOperators(output = levelView.operators[0],input = levelView.operators[1])
         connectOperators(output = levelView.operators[1],input = levelView.operators[2])
         connectOperators(output = levelView.operators[1],input = levelView.operators[3])
+
+        thread = Thread(Runnable {
+            while (true) {
+                if (levelView.operators[0].state == 1) {
+                    levelView.win = true
+                    println("win")
+                    break
+                }
+            }
+        })
+        thread.start()
     }
 }
