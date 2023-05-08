22f2e21baf9b3346ef3055720384e250fb95c377
Quentin Debroux <132827056+QuentinDebroux@users.noreply.github.com>
2023-05-08 19:51:50 +0200

    Update Level.kt

--- app/src/main/java/com/example/logicgateappproject/levels/Level.kt
+++ app/src/main/java/com/example/logicgateappproject/levels/Level.kt
@@ -1,45 +1,42 @@
 package com.example.logicgateappproject.levels
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.logicgateappproject.LevelsMenu
-import com.example.logicgateappproject.R
 import com.example.logicgateappproject.operators.*
 
-abstract class Level: AppCompatActivity() {
-
-    protected lateinit var levelView: LevelView
+class Level2: Level() {
+    private var h: Float = 1f
+    private var w: Float = 1f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.level)
-        levelView = findViewById<LevelView>(R.id.vLevels)
-        declareOperators()
+        h = levelView.screenHeight
+        w = levelView.screenWidth
+        levelView.levelName = "Level 2"
     }
 
-    abstract fun declareOperators()
+    override fun declareOperators() {
+        //Create operators and put them in the list
+        levelView.operators.add(ExactLight(w/2, h/4, this, 1, 1))
 
-    fun connectOperators(output: Operator, input: Operator) {
-        when(output) {
-            is LogicGate -> { output.connectIn(input) }
-            is Light -> { output.connectIn(input) }
-        }
-    }
+        levelView.operators.add(And(w/2, h/2, this))
 
-    override fun onPause() {
-        super.onPause()
-        levelView.pause()
-    }
+        levelView.operators.add(NormalSwitch(w/3, 3*h/4, this))
+        levelView.operators.add(NormalSwitch(2*w/3, 3*h/4, this))
 
-    override fun onResume() {
-        super.onResume()
-        levelView.resume()
-    }
+        //Connect operators
+        connectOperators(output = levelView.operators[0],input = levelView.operators[1])
+        connectOperators(output = levelView.operators[1],input = levelView.operators[2])
+        connectOperators(output = levelView.operators[1],input = levelView.operators[3])
 
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        val intent = Intent(this, LevelsMenu::class.java)
-        startActivity(intent)
+        thread = Thread(Runnable {
+            while (true) {
+                if (levelView.operators[0].state == 1) {
+                    levelView.win = true
+                    println("win")
+                    break
+                }
+            }
+        })
+        thread.start()
     }
 }
