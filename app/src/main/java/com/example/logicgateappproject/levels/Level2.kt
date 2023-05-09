025f8bb9566821fb2272e8b1abcc78466793a7af
Quentin Debroux <132827056+QuentinDebroux@users.noreply.github.com>
2023-05-09 20:50:14 +0200

    Update Level2.kt

--- app/src/main/java/com/example/logicgateappproject/levels/Level2.kt
+++ app/src/main/java/com/example/logicgateappproject/levels/Level2.kt
@@ -4,31 +4,40 @@ import android.os.Bundle
 import com.example.logicgateappproject.operators.*
 
 class Level2: Level() {
-    private var h: Float = 1f   // Height of the screen
-    private var w: Float = 1f   // Width of the screen
+    private var h: Float = 1f
+    private var w: Float = 1f
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)  // Call the super class constructor to create the instance of the level
-        h = levelView.screenHeight  // Get the height of the screen
-        w = levelView.screenWidth   // Get the width of the screen
+        super.onCreate(savedInstanceState)
+        h = levelView.screenHeight
+        w = levelView.screenWidth
         levelView.levelName = "Level 2"
     }
 
     override fun declareOperators() {
         //Create operators and put them in the list
-        levelView.operators.add(ExactLight(w/2, h/4, this, 1, 1))
+        levelView.operators.add(MoreThanLight(w/2, h/4, this, 3, 2)) //0
 
-        levelView.operators.add(And(w/2, h/2, this))
+        levelView.operators.add(Xor(2.5f*w/7, 5*h/8, this)) //1
+        levelView.operators.add(And(4.5f*w/7, 5*h/8, this)) //2
 
-        levelView.operators.add(NormalSwitch(w/3, 3*h/4, this))
-        levelView.operators.add(NormalSwitch(2*w/3, 3*h/4, this))
+        levelView.operators.add(ClockSwitch(w/6, h/2, this)) //3
+        levelView.operators.add(NormalSwitch(w/4, 3*h/4, this)) //4
+        levelView.operators.add(TimerSwitch(w/2, 3*h/4, this)) //5
+        levelView.operators.add(TimerSwitch(3*w/4, 3*h/4, this)) //6
 
         //Connect operators
+        connectOperators(output = levelView.operators[0],input = levelView.operators[3])
         connectOperators(output = levelView.operators[0],input = levelView.operators[1])
-        connectOperators(output = levelView.operators[1],input = levelView.operators[2])
-        connectOperators(output = levelView.operators[1],input = levelView.operators[3])
+        connectOperators(output = levelView.operators[0],input = levelView.operators[2])
 
-        thread = Thread(Runnable {  // Create a thread to check if the level is won
+        connectOperators(output = levelView.operators[1],input = levelView.operators[4])
+        connectOperators(output = levelView.operators[1],input = levelView.operators[5])
+        connectOperators(output = levelView.operators[2],input = levelView.operators[5])
+        connectOperators(output = levelView.operators[2],input = levelView.operators[6])
+
+        //verifies if the level is cleared
+        thread = Thread(Runnable {
             while (true) {
                 if (levelView.operators[0].state == 1) {
                     levelView.win = true
